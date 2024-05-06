5563d624a1c68b57897dcac6a9b89a363bf4e472
Ekoramadhan13 <enugraha411@gmail.com>
2024-05-06 10:56:57 +0700

    int main

--- pemdas0605/pemdas0605.cpp
+++ pemdas0605/pemdas0605.cpp
@@ -9,3 +9,19 @@ class mahasiswa {
 	}
 };
 
+int main()
+{
+	mahasiswa mhs{ 1 }; //obkect mhs
+	mhs.showNim(); //member Acces operator
+
+	mahasiswa& refmhs = mhs; //pointer reference refmhs
+	refmhs.nim = 2; //member access operator
+	mhs.showNim();
+
+	mahasiswa* pmhs = &mhs;
+	pmhs->nim = 3;
+	mhs.showNim();
+	return 0;
+
+}
+
